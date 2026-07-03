d2e728a57a0e300f79d275e21a7b86cba26077bf
Swedaj Deshmukh <deshmukhswedaj@gmail.com>
2026-07-04 01:12:15 +0530

    Reactivity App Walking Skeleton for .net

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Domain;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Activity> Activities { get; set; }
+    }
+}
